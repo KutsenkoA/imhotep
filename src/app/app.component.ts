9a897995ed2790fd358d2e131610ea918c13497c
Kutsenko Andrey <andr.donetsk@gmail.com>
2018-10-30 09:50:14 +0100

    add swagger-ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwaggerUI } from 'swagger-ui';
 
 @Component({
   selector: 'imho-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'imhotep';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    SwaggerUI({
+      dom_id: '#swagger-ui',
+      urls: [{
+        url: 'url',
+        name: 'API'
+      }]
+    });
+  }
+
 }
